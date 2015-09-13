5c38f4e978eec182b8af7dd513b074934ba906e6
Dev Jha <Dev Jha@DevJha-PC>
2015-09-13 19:46:05 +0530

    managedBean

---
+++ src/com/getPrint/notes/web/NotesManagedBean.java
@@ -0,0 +1,68 @@
+package com.getPrint.notes.web;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import ManagedBean;
+import ObjectMapper;
+import RequestScoped;
+import Response;
+
+
+@ManagedBean(name="notesManagedBean")
+@RequestScoped
+public class NotesManagedBean {
+	
+	 
+	 
+	 
+	    public int noteId;
+		private String title; 
+		private String notes;
+		private Date createTime;
+		private Date lastUpdateTime;
+		
+		@PostConstruct
+	    public void init() {
+	       
+	    }	
+		 
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public String getNotes() {
+			return notes;
+		}
+		public void setNotes(String notes) {
+			this.notes = notes;
+		}
+		public Date getCreateTime() {
+			return createTime;
+		}
+		public void setCreateTime(Date createTime) {
+			this.createTime = createTime;
+		}
+		public Date getLastUpdateTime() {
+			return lastUpdateTime;
+		}
+		public void setLastUpdateTime(Date lastUpdateTime) {
+			this.lastUpdateTime = lastUpdateTime;
+		}
+		public int getNoteId() {
+			return noteId;
+		}
+		public void setNoteId(int noteId) {
+			this.noteId = noteId;
+		}
+		
+	 
+	
+
+}
