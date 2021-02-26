16551545f60beff5fa91b9df3024dcef9f768105
deven <devendra.jha@flex.com>
2021-02-26 19:13:17 +0530

    added new function for validation

--- src/com/getPrint/notes/web/NotesManagedBean.java
+++ src/com/getPrint/notes/web/NotesManagedBean.java
@@ -12,70 +12,74 @@ import ObjectMapper;
 import RequestScoped;
 import Response;
 
-
-@ManagedBean(name="notesManagedBean")
+@ManagedBean(name = "notesManagedBean")
 @RequestScoped
 public class NotesManagedBean {
-	
-	 
-	 
-	 
-	    public int noteId;
-		private String title; 
-		private String notes;
-		private Date createTime;
-		private Date lastUpdateTime;
-		
-		@PostConstruct
-	    public void init() {
-	       
-	    }	
-		 
-		public String getTitle() {
-			return title;
-		}
-		public void setTitle(String title) {
-			this.title = title;
-		}
-		public String getNotes() {
-			return notes;
-		}
-		public void setNotes(String notes) {
-			this.notes = notes;
-		}
-		public Date getCreateTime() {
-			return createTime;
-		}
-		public void setCreateTime(Date createTime) {
-			this.createTime = createTime;
-		}
-		public Date getLastUpdateTime() {
-			return lastUpdateTime;
-		}
-		public void setLastUpdateTime(Date lastUpdateTime) {
-			this.lastUpdateTime = lastUpdateTime;
-		}
-		public int getNoteId() {
-			return noteId;
-		}
-		public void setNoteId(int noteId) {
-			this.noteId = noteId;
-		}
-		
-	 public void addnotes() {
-		   final StringBuilder url;
 
-		   final Client client = ClientBuilder.newClient();
-	        final WebTarget target = client.target(url);
-	        final Response response = target.request().header("JSESSIONID", accessToken)
-	                .accept(Constants.CONTENT_TYPE).get();
+	public int noteId;
+	private String title;
+	private String notes;
+	private Date createTime;
+	private Date lastUpdateTime;
 
-	        response.bufferEntity();
-	        client.close();
+	@PostConstruct
+	public void init() {
 
-	        return (response);
-		
 	}
-	
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getLastUpdateTime() {
+		return lastUpdateTime;
+	}
+
+	public void setLastUpdateTime(Date lastUpdateTime) {
+		this.lastUpdateTime = lastUpdateTime;
+	}
+
+	public int getNoteId() {
+		return noteId;
+	}
+
+	public void setNoteId(int noteId) {
+		this.noteId = noteId;
+	}
+
+	public void addnotes() {
+		final StringBuilder url;
+
+		final Client client = ClientBuilder.newClient();
+		final WebTarget target = client.target(url);
+		final Response response = target.request().header("JSESSIONID", accessToken).accept(Constants.CONTENT_TYPE)
+				.get();
+
+		response.bufferEntity();
+		client.close();
+
+		return (response);
+
+	}
 
 }
