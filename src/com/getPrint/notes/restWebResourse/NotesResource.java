7ab6bbceac6acf6391334dd38a7c28f5c97877bd
deven <devendra.jha@flex.com>
2021-02-26 19:20:37 +0530

    hello

--- src/com/getPrint/notes/restWebResourse/NotesResource.java
+++ src/com/getPrint/notes/restWebResourse/NotesResource.java
@@ -1,6 +1,5 @@
 package com.getPrint.notes.restWebResourse;
 
-
 import java.util.List;
 
 import javax.ws.rs.DELETE;
@@ -20,66 +19,58 @@ import com.getPrint.notes.exception.AppException;
 import com.getPrint.notes.util.HibernateTemplate;
 import com.getPrint.notes.util.NotesVO;
 
-
 @Path("/notes")
 public class NotesResource {
 
-	@Autowired 
+	@Autowired
 	public NotesDAO notesDAO;
-	
-		@POST
-		@Produces(MediaType.APPLICATION_JSON)
-		public Response addNotes(@PathParam("notes") NotesVO notes,@PathParam("userId") int userId)
-				throws AppException {
-			
-			notesDAO.addNotes(notes, userId);
-			
-			return Response.ok(notes).build();
-		}
 
-		@PUT
-		@Produces(MediaType.APPLICATION_JSON)
-		public Response updateNotes(@PathParam("notes") NotesVO notes,@PathParam("userId") int userId)
-				throws AppException {
-			notesDAO.updateNotes(notes);
-			
-			return Response.ok(notes).build();
-		}
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addNotes(@PathParam("notes") NotesVO notes, @PathParam("userId") int userId) throws AppException {
 
-	
-		@GET
-		@Path("id/{userId}")
-		@Produces(MediaType.APPLICATION_JSON)
-		public Response getNotes(@PathParam("cls") Class cls,
-				@PathParam("userId") int userId) throws AppException {
-			List<Object> notesList = null;
-			try{
-				notesList= (List<Object>) notesDAO.getNode(cls, userId);
-				throw new AppException();
-			}catch (AppException e) {
-				
-				e.getMessage();
-			}
-		if (notesList == null || notesList.isEmpty()) {
-				throw new AppException();
+		notesDAO.addNotes(notes, userId);
 
-			}
-			
-			
-
-				
-			return Response.ok(notesList).build();
-
-		}
-		
-		@DELETE
-		@Produces(MediaType.APPLICATION_JSON)
-		@Path("deleteRecord/{noteId}")
-		public Response deleteNotes(@PathParam("noteId") int noteId) throws AppException {
-			notesDAO.deleteNotes(noteId);
-			return Response.ok("true").build();
-
-		}
-
-	
+		return Response.ok(notes).build();
 	}
+
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response updateNotes(@PathParam("notes") NotesVO notes, @PathParam("userId") int userId)
+			throws AppException {
+		notesDAO.updateNotes(notes);
+
+		return Response.ok(notes).build();
+	}
+
+	@GET
+	@Path("id/{userId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getNotes(@PathParam("cls") Class cls, @PathParam("userId") int userId) throws AppException {
+		List<Object> notesList = null;
+		try {
+			notesList = (List<Object>) notesDAO.getNode(cls, userId);
+			throw new AppException();
+		} catch (AppException e) {
+
+			e.getMessage();
+		}
+		if (notesList == null || notesList.isEmpty()) {
+			throw new AppException();
+
+		}
+
+		return Response.ok(notesList).build();
+
+	}
+
+	@DELETE
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("deleteRecord/{noteId}")
+	public Response deleteNotes(@PathParam("noteId") int noteId) throws AppException {
+		notesDAO.deleteNotes(noteId);
+		return Response.ok("true").build();
+
+	}
+
+}
